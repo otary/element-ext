4673392b6ddb66d193075fb77c6efd4a8c0f7a1a
chenzw <656469722@qq.com>
2019-10-15 20:43:45 +0800

    initialization

--- src/util/url.js
+++ src/util/url.js
@@ -1,21 +1,45 @@
 /**
+ * 获取url参数
+ * @param paramName
+ * @param url
+ * @returns {string}
+ */
+function getUriParam(paramName, url = window.location.href) {
+    const reg = new RegExp("[?&]" + paramName + "=([^&#]*)", "i");
+    const results = url.match(reg);
+    if (results && results.length > 1) {
+        return decodeURIComponent(results[1]);
+    }
+    return '';
+}
+
+/**
+ * 获取url参数列表（数组）
+ * @param paramName
+ * @param url
+ * @returns {string}
+ */
+function getUriParams(paramName, url = window.location.href) {
+    let searchParams = new URLSearchParams(url);
+    return searchParams.getAll(paramName);
+}
+
+/**
+ * 是否存在指定的参数
+ * @param paramName
+ * @param url
+ * @returns {boolean}
+ */
+function existsParam(paramName, url = window.location.href) {
+    let searchParams = new URLSearchParams(url);
+    return searchParams.has(paramName);
+}
+
+/**
  * @author chenzw
  */
 module.exports = {
-    /**
-     * 获取url参数
-     * @param paramName
-     * @param url
-     * @returns {string}
-     */
-    getUriParam: (paramName, url = window.location.href) => {
-        const reg = new RegExp("[?&]" + paramName + "=([^&#]*)", "i");
-        const results = url.match(reg);
-        if (results && results.length > 1) {
-            return decodeURIComponent(results[1]);
-        }
-        return '';
-    }
-
-
+    getUriParam,
+    getUriParams,
+    existsParam
 }
