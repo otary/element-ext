293f9abd2ce2ef2612ae6582246d5410cfef378f
chenzw <656469722@qq.com>
2019-11-07 00:03:40 +0800

    initialization

---
+++ docs/README.md
@@ -0,0 +1,145 @@
+# element-ext
+
+## 样式类
+
+统一以 `czw-` 开头
+
+> 美化滚动条
+
+兼容性    
+- [x] chrome
+- [x] edge
+
+``` html
+<div class="czw-scrollbar-beautify-6">
+
+</div>
+```
+
+> 隐藏滚动条
+
+``` html
+<div class="czw-scrollbar-beautify-hide">
+
+</div>
+```
+
+> 遮罩
+
+``` html
+<div class="czw-mask-absoulte">
+</div>
+```
+
+> 
+
+---
+
+## 工具类
+
+### string
+
+- 驼峰/Pascal转下划线格式
+
+```
+import {string} from 'element-ext';
+
+string.toCamel('UserName');  // => user_name
+string.toCamel("UserName", '#'); // => user#name;
+```
+
+### array
+
+- 判断是否数组
+
+```
+import {array} from 'element-ext';
+
+array.isArray([1,2]); // => true
+```
+
+### object
+
+- 克隆
+
+```
+import {object} from 'element-ext';
+
+let o1 = {
+    a: 1, b: 2, c: {
+        c1: 3,
+        c2: 4
+    }
+};
+
+// 浅复制
+let o2 = object.clone(o1);  
+o1 === o2 // => false
+
+// 深复制
+let o3 = object.cloneDeep(o1);  
+o1 === o3 // => false
+
+```
+
+- 判断对象的属性是否满足某个条件
+
+```
+import {object} from 'element-ext';
+
+object.conformsTo({a: 1, b: 2}, {
+    'a': function (n) {
+        return n == 1;
+    }
+}); // => true
+```
+
+
+### url
+
+- 获取url参数
+
+```
+import {url} from 'element-ext';
+
+url.getUriParam('username', 'http://www.baidu.com?username=chenzw');  // => chenzw
+```
+
+### dom
+
+- 动态加载css资源
+
+```
+import {dom} from 'element-ext';
+dom.loadCss('./theme.css');
+```
+
+- 动态加载js资源
+
+```
+import {dom} from 'element-ext';
+dom.loadScript('./jquery.js');
+```
+
+---
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
--- examples/views/Home.vue
+++ examples/views/Home.vue
@@ -4,8 +4,8 @@
         <!-- 滚动条示例 -->
         <fieldset>
             <legend>滚动条美化</legend>
-            <div style="height: 100px; overflow: auto;" class="czw-border czw-scrollbar-beautify">
-                <div style="height: 2000px;">
+            <div style="width: 500px; height: 100px; overflow: auto;" class="czw-border czw-scrollbar-beautify-6">
+                <div style="width: 2000px;height: 2000px;">
                     内容
                 </div>
             </div>
@@ -21,11 +21,31 @@
         </fieldset>
 
         <fieldset>
-            <legend>遮罩</legend>
-            <div style="height: 100px;">
-                <div class="czw-mask-absoulte"></div>
+            <legend>区域遮罩</legend>
+            <div style="position: relative; height: 100px;">
+                <div class="czw-mask-absolute"></div>
             </div>
         </fieldset>
+
+        <fieldset>
+            <legend>全局遮罩</legend>
+            <div>
+                {{maskFixedHtml}}
+            </div>
+        </fieldset>
+
+        <fieldset>
+            <legend>七彩颜色条</legend>
+            <div style="height: 5px;" class="czw-bg-colorful"></div>
+        </fieldset>
+
+        <fieldset>
+            <legend>七彩边框</legend>
+            <div style="border: 2px solid transparent;" class="czw-border-colorful">
+                内容
+            </div>
+        </fieldset>
+
 
 
         <czw-link>ssdfsdfsd</czw-link>
@@ -46,7 +66,6 @@
         </div>
 
 
-
         <div class="dot">
 
         </div>
@@ -56,10 +75,13 @@
 
 <script>
 
+
     export default {
         name: "Home",
         data() {
-            return {}
+            return {
+                maskFixedHtml:`<div class="czw-mask-fixed"></div>`
+            }
         },
         components: {
             "czw-link": () => import("@/components/Link")
@@ -79,8 +101,9 @@
         position: absolute;
         border-radius: 50%;
     }
+
     .pulse {
-        background: rgba(0,0,0,0.2);
+        background: rgba(0, 0, 0, 0.2);
         border-radius: 50%;
         height: 14px;
         width: 14px;
@@ -95,6 +118,7 @@
         transform: rotateX(55deg);
         z-index: -2;
     }
+
     .pulse:after {
         content: "";
         border-radius: 50%;
@@ -125,8 +149,7 @@
     }
 
 
-
-    .dot{
+    .dot {
         width: 10px;
         height: 10px;
         background-color: #ccc;
@@ -134,14 +157,14 @@
 
         animation: pulsate 1s ease-out infinite;
 
-      /*  box-shadow: 0 0 1px 2px #89849b;*/
+        /*  box-shadow: 0 0 1px 2px #89849b;*/
     }
 
     @keyframes pulsate {
         0% {
             transform: scale(0.1, 0.1);
             opacity: 0;
-           /* -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";*/
+            /* -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";*/
             filter: alpha(opacity=0);
         }
         50% {
@@ -164,115 +187,115 @@
     }
 
 
-  /*  @-moz-keyframes bounce {
-        0% {
-            opacity: 0;
-            -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
-            filter: alpha(opacity=0);
-            -webkit-transform: translateY(-2000px) rotate(-45deg);
-            -moz-transform: translateY(-2000px) rotate(-45deg);
-            -o-transform: translateY(-2000px) rotate(-45deg);
-            -ms-transform: translateY(-2000px) rotate(-45deg);
-            transform: translateY(-2000px) rotate(-45deg);
-        }
-        60% {
-            opacity: 1;
-            -ms-filter: none;
-            filter: none;
-            -webkit-transform: translateY(30px) rotate(-45deg);
-            -moz-transform: translateY(30px) rotate(-45deg);
-            -o-transform: translateY(30px) rotate(-45deg);
-            -ms-transform: translateY(30px) rotate(-45deg);
-            transform: translateY(30px) rotate(-45deg);
-        }
-        80% {
-            -webkit-transform: translateY(-10px) rotate(-45deg);
-            -moz-transform: translateY(-10px) rotate(-45deg);
-            -o-transform: translateY(-10px) rotate(-45deg);
-            -ms-transform: translateY(-10px) rotate(-45deg);
-            transform: translateY(-10px) rotate(-45deg);
-        }
-        100% {
-            -webkit-transform: translateY(0) rotate(-45deg);
-            -moz-transform: translateY(0) rotate(-45deg);
-            -o-transform: translateY(0) rotate(-45deg);
-            -ms-transform: translateY(0) rotate(-45deg);
-            transform: translateY(0) rotate(-45deg);
-        }
-    }
-    @-webkit-keyframes bounce {
-        0% {
-            opacity: 0;
-            -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
-            filter: alpha(opacity=0);
-            -webkit-transform: translateY(-2000px) rotate(-45deg);
-            -moz-transform: translateY(-2000px) rotate(-45deg);
-            -o-transform: translateY(-2000px) rotate(-45deg);
-            -ms-transform: translateY(-2000px) rotate(-45deg);
-            transform: translateY(-2000px) rotate(-45deg);
-        }
-        60% {
-            opacity: 1;
-            -ms-filter: none;
-            filter: none;
-            -webkit-transform: translateY(30px) rotate(-45deg);
-            -moz-transform: translateY(30px) rotate(-45deg);
-            -o-transform: translateY(30px) rotate(-45deg);
-            -ms-transform: translateY(30px) rotate(-45deg);
-            transform: translateY(30px) rotate(-45deg);
-        }
-        80% {
-            -webkit-transform: translateY(-10px) rotate(-45deg);
-            -moz-transform: translateY(-10px) rotate(-45deg);
-            -o-transform: translateY(-10px) rotate(-45deg);
-            -ms-transform: translateY(-10px) rotate(-45deg);
-            transform: translateY(-10px) rotate(-45deg);
-        }
-        100% {
-            -webkit-transform: translateY(0) rotate(-45deg);
-            -moz-transform: translateY(0) rotate(-45deg);
-            -o-transform: translateY(0) rotate(-45deg);
-            -ms-transform: translateY(0) rotate(-45deg);
-            transform: translateY(0) rotate(-45deg);
-        }
-    }
-    @-o-keyframes bounce {
-        0% {
-            opacity: 0;
-            -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
-            filter: alpha(opacity=0);
-            -webkit-transform: translateY(-2000px) rotate(-45deg);
-            -moz-transform: translateY(-2000px) rotate(-45deg);
-            -o-transform: translateY(-2000px) rotate(-45deg);
-            -ms-transform: translateY(-2000px) rotate(-45deg);
-            transform: translateY(-2000px) rotate(-45deg);
-        }
-        60% {
-            opacity: 1;
-            -ms-filter: none;
-            filter: none;
-            -webkit-transform: translateY(30px) rotate(-45deg);
-            -moz-transform: translateY(30px) rotate(-45deg);
-            -o-transform: translateY(30px) rotate(-45deg);
-            -ms-transform: translateY(30px) rotate(-45deg);
-            transform: translateY(30px) rotate(-45deg);
-        }
-        80% {
-            -webkit-transform: translateY(-10px) rotate(-45deg);
-            -moz-transform: translateY(-10px) rotate(-45deg);
-            -o-transform: translateY(-10px) rotate(-45deg);
-            -ms-transform: translateY(-10px) rotate(-45deg);
-            transform: translateY(-10px) rotate(-45deg);
-        }
-        100% {
-            -webkit-transform: translateY(0) rotate(-45deg);
-            -moz-transform: translateY(0) rotate(-45deg);
-            -o-transform: translateY(0) rotate(-45deg);
-            -ms-transform: translateY(0) rotate(-45deg);
-            transform: translateY(0) rotate(-45deg);
-        }
-    }
-    */
+    /*  @-moz-keyframes bounce {
+          0% {
+              opacity: 0;
+              -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
+              filter: alpha(opacity=0);
+              -webkit-transform: translateY(-2000px) rotate(-45deg);
+              -moz-transform: translateY(-2000px) rotate(-45deg);
+              -o-transform: translateY(-2000px) rotate(-45deg);
+              -ms-transform: translateY(-2000px) rotate(-45deg);
+              transform: translateY(-2000px) rotate(-45deg);
+          }
+          60% {
+              opacity: 1;
+              -ms-filter: none;
+              filter: none;
+              -webkit-transform: translateY(30px) rotate(-45deg);
+              -moz-transform: translateY(30px) rotate(-45deg);
+              -o-transform: translateY(30px) rotate(-45deg);
+              -ms-transform: translateY(30px) rotate(-45deg);
+              transform: translateY(30px) rotate(-45deg);
+          }
+          80% {
+              -webkit-transform: translateY(-10px) rotate(-45deg);
+              -moz-transform: translateY(-10px) rotate(-45deg);
+              -o-transform: translateY(-10px) rotate(-45deg);
+              -ms-transform: translateY(-10px) rotate(-45deg);
+              transform: translateY(-10px) rotate(-45deg);
+          }
+          100% {
+              -webkit-transform: translateY(0) rotate(-45deg);
+              -moz-transform: translateY(0) rotate(-45deg);
+              -o-transform: translateY(0) rotate(-45deg);
+              -ms-transform: translateY(0) rotate(-45deg);
+              transform: translateY(0) rotate(-45deg);
+          }
+      }
+      @-webkit-keyframes bounce {
+          0% {
+              opacity: 0;
+              -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
+              filter: alpha(opacity=0);
+              -webkit-transform: translateY(-2000px) rotate(-45deg);
+              -moz-transform: translateY(-2000px) rotate(-45deg);
+              -o-transform: translateY(-2000px) rotate(-45deg);
+              -ms-transform: translateY(-2000px) rotate(-45deg);
+              transform: translateY(-2000px) rotate(-45deg);
+          }
+          60% {
+              opacity: 1;
+              -ms-filter: none;
+              filter: none;
+              -webkit-transform: translateY(30px) rotate(-45deg);
+              -moz-transform: translateY(30px) rotate(-45deg);
+              -o-transform: translateY(30px) rotate(-45deg);
+              -ms-transform: translateY(30px) rotate(-45deg);
+              transform: translateY(30px) rotate(-45deg);
+          }
+          80% {
+              -webkit-transform: translateY(-10px) rotate(-45deg);
+              -moz-transform: translateY(-10px) rotate(-45deg);
+              -o-transform: translateY(-10px) rotate(-45deg);
+              -ms-transform: translateY(-10px) rotate(-45deg);
+              transform: translateY(-10px) rotate(-45deg);
+          }
+          100% {
+              -webkit-transform: translateY(0) rotate(-45deg);
+              -moz-transform: translateY(0) rotate(-45deg);
+              -o-transform: translateY(0) rotate(-45deg);
+              -ms-transform: translateY(0) rotate(-45deg);
+              transform: translateY(0) rotate(-45deg);
+          }
+      }
+      @-o-keyframes bounce {
+          0% {
+              opacity: 0;
+              -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
+              filter: alpha(opacity=0);
+              -webkit-transform: translateY(-2000px) rotate(-45deg);
+              -moz-transform: translateY(-2000px) rotate(-45deg);
+              -o-transform: translateY(-2000px) rotate(-45deg);
+              -ms-transform: translateY(-2000px) rotate(-45deg);
+              transform: translateY(-2000px) rotate(-45deg);
+          }
+          60% {
+              opacity: 1;
+              -ms-filter: none;
+              filter: none;
+              -webkit-transform: translateY(30px) rotate(-45deg);
+              -moz-transform: translateY(30px) rotate(-45deg);
+              -o-transform: translateY(30px) rotate(-45deg);
+              -ms-transform: translateY(30px) rotate(-45deg);
+              transform: translateY(30px) rotate(-45deg);
+          }
+          80% {
+              -webkit-transform: translateY(-10px) rotate(-45deg);
+              -moz-transform: translateY(-10px) rotate(-45deg);
+              -o-transform: translateY(-10px) rotate(-45deg);
+              -ms-transform: translateY(-10px) rotate(-45deg);
+              transform: translateY(-10px) rotate(-45deg);
+          }
+          100% {
+              -webkit-transform: translateY(0) rotate(-45deg);
+              -moz-transform: translateY(0) rotate(-45deg);
+              -o-transform: translateY(0) rotate(-45deg);
+              -ms-transform: translateY(0) rotate(-45deg);
+              transform: translateY(0) rotate(-45deg);
+          }
+      }
+      */
 
 
     @keyframes bounce {
@@ -315,8 +338,6 @@
 
 <style scoped lang="scss">
     @import "~@scss/index";
-
-
 
 
     .test2 {
