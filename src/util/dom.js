4cbb2638ca3580bc90dea1101d2b5fab18db40cf
chenzw <656469722@qq.com>
2019-11-07 22:00:01 +0800

    initialization

--- src/util/dom.js
+++ src/util/dom.js
@@ -2,7 +2,7 @@
  * 动态加载css资源
  * @param url
  */
-function loadCss(url) {
+const loadCss = (url) => {
     let tag = document.createElement('link');
     for (let i = 0; i < document.links.length; i++) {
         if (document.links[0].href == url) {
@@ -14,13 +14,13 @@ function loadCss(url) {
     tag.setAttribute("href", url);
 
     document.body.appendChild(tag);
-}
+};
 
 /**
  * 加载js资源
  * @param url
  */
-function loadScript(url) {
+const loadScript = (url) => {
     let tag = document.createElement('script');
     for (let i = 0; i < document.scripts.length; i++) {
         if (document.scripts[0].src == url) {
@@ -30,24 +30,30 @@ function loadScript(url) {
     tag.setAttribute("src", url);
 
     document.body.appendChild(tag);
-}
+};
 
-function delayInit(o, callback) {
-    if (typeof o == 'undefined') {
+/**
+ * 延迟阻塞
+ *
+ * @param matchFn
+ * @param callbackFn
+ */
+const delayInit = (matchFn, callbackFn) => {
+    if (matchFn()) {
         let timer = setInterval(function () {
-            if (typeof o == 'undefined') {
-                return false;
+            if (matchFn()) {
+                clearInterval(timer);
+                return callbackFn();
             }
-            clearInterval(timer);
-            callback(o);
         }, 50);
     } else {
-        callback(o);
+        return callbackFn();
     }
-}
+};
 
 
 module.exports = {
     loadCss,
-    loadScript
+    loadScript,
+    delayInit
 }
---
+++ test/util/dom.test.js
@@ -0,0 +1,19 @@
+const {dom} = require('../../src/util/index.js');
+
+/**
+ * 延迟阻塞
+ */
+test('dom.delayInit', () => {
+
+    let loadFinished = false;
+    window.setTimeout(() => {
+        loadFinished = true;
+    }, 2000);
+
+    expect(dom.delayInit(() => {
+        return loadFinished;
+    }, () => {
+        return 'finish';
+    })).toBe('finish');
+
+});
