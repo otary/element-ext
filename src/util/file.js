30b1bce9f83ae3b4ada2dfd352642d5bfabb6083
chenzw <656469722@qq.com>
2021-09-25 19:08:14 +0800

    Merge branch 'master' of github.com:otary/element-ext
    # Conflicts:
    #	src/util/index.js
    #	test/util/number.test.js

--- src/util/file.js
+++ src/util/file.js
@@ -2,7 +2,7 @@
  * 获取文件后缀名
  * @param fileName
  */
-function getFileExtension(fileName) {
+export function getFileExtension(fileName) {
     return fileName.split('.').pop();
 }
 
@@ -11,11 +11,42 @@ function getFileExtension(fileName) {
  * @param fileName
  * @returns {string}
  */
-function getFileName(fileName) {
+export function getFileName(fileName) {
     return fileName.substring(fileName.lastIndexOf('/') + 1, fileName.lastIndexOf("."));
+}
+
+/**
+ * 文件转DataURL
+ * @param file
+ * @returns {Promise<any>}
+ */
+export function file2DataURL(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+
+/**
+ * DataURL 转 File对象
+ * @param dataURL
+ * @param fileName
+ * @returns {File}
+ */
+export function dataURL2File(dataURL, fileName = '') {
+    var arr = dataURL.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], fileName, {type:mime});
 }
 
 module.exports = {
     getFileExtension,
-    getFileName
+    getFileName,
+    file2DataURL,
+    dataURL2File
 };
