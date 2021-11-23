88380d1e77d91ffb671c840c451c45653a7a997d
chenzw <656469722@qq.com>
2021-11-23 20:26:14 +0800

    update

--- src/util/screen.js
+++ src/util/screen.js
@@ -1,17 +1,28 @@
 /**
- * 获取PPI
+ * 获取屏幕信息
+ * @returns {{inDevice: number, ppi: number, inHeight: number, inWidth: number, cunDevice: number}}
  */
-function getPPI() {
+function getScreenInfo() {
     const el = document.createElement('div');
     el.style.width = "1in";
     el.style.padding = '0';
     document.body.appendChild(el);
     const ppi = el.offsetWidth;
     document.body.removeChild(el);
-    return ppi;
+    const inWidth = window.screen.width / ppi;
+    const inHeight = window.screen.height / ppi;
+    const inDevice = Math.round(Math.sqrt(inWidth * inWidth + inHeight * inHeight));
+    const cunDevice = Math.round(Math.sqrt(inWidth * inWidth + inHeight * inHeight) * 0.00762);
+
+    return {
+        ppi,
+        inWidth,  // 屏幕宽度（英寸）
+        inHeight,  // 屏幕高度（英寸）
+        inDevice,  // 屏幕尺寸（英寸）
+        cunDevice,  // 屏幕尺寸（寸）
+    }
 }
 
-
 module.exports = {
-    getPPI
+    getScreenInfo
 }
