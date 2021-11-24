9f24dedf89591585a8aa6fca6f3737a01078b26f
chenzw <656469722@qq.com>
2021-11-24 11:41:43 +0800

    update

--- src/util/number.js
+++ src/util/number.js
@@ -112,7 +112,7 @@ function div(a, b) {
  * @returns {number}
  */
 function multiply(a, b) {
-    const m = getDecimalDigits(a) +  getDecimalDigits(b);
+    const m = getDecimalDigits(a) + getDecimalDigits(b);
     const ia = Number(a.toString().replace(".", ""));
     const ib = Number(b.toString().replace(".", ""));
     return ia * ib / Math.pow(10, m);
@@ -132,6 +132,77 @@ function crossNum(num, maxNum, suffix = '+') {
     return n;
 }
 
+/**
+ * 数字 => 中文金钱大写
+ * @param num
+ * @returns {string}
+ */
+function toChineseMoney(num) {
+    const cnNums = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"]; // 汉字的数字
+    const cnRadices = ["", "拾", "佰", "仟"]; // 基数单位
+    const cnUnits = ["", "万", "亿", "兆"];  // 大数单位
+    const cnDecimalUnits = ["角", "分", "毫", "厘"];  // 小数部分单位
+    const cnIntSuffix = "整"; // 整数后缀
+    const cnIntYuan = "元";
+    let result = "";
+
+    if (num === "") {
+        return "";
+    }
+
+    if (parseFloat(num) === 0) {
+        return cnNums[0] + cnIntYuan + cnIntSuffix;
+    }
+
+    // 分离整数和小数部分
+    const numParts = num.toString().split(".");
+    const intPart = numParts[0];
+    let decimalPart = '';
+    if (numParts.length > 1) {
+        decimalPart = numParts[1].substring(0, 4); // 只支持到厘
+    }
+
+    // 整数部分处理
+    if (parseInt(intPart, 10) > 0) {
+        let zeroCount = 0;
+        for (let i = 0; i < intPart.length; i++) {
+            const current = intPart.substr(i, 1);
+
+            const p = intPart.length - i - 1;
+            const unitIndex = p / 4;
+            const radiceIndex = p % 4;
+
+            if (current == "0") {
+                zeroCount++;
+            } else {
+                if (zeroCount > 0) {
+                    result += cnNums[0];
+                }
+                zeroCount = 0;
+                result += cnNums[parseInt(current)] + cnRadices[radiceIndex];
+            }
+
+            if (radiceIndex == 0 && zeroCount < 4) {
+                result += cnUnits[unitIndex];
+            }
+        }
+        result += cnIntYuan;
+    }
+
+    // 小数部分处理
+    if (decimalPart != '') {
+        for (let i = 0; i < decimalPart.length; i++) {
+            const current = decimalPart.substr(i, 1);
+            if (current != "0") {
+                result += cnNums[parseInt(current)] + cnDecimalUnits[i];
+            }
+        }
+    } else {
+        result += cnIntSuffix;
+    }
+    return result;
+}
+
 
 module.exports = {
     intToHex,
@@ -143,5 +214,6 @@ module.exports = {
     sub,
     div,
     multiply,
-    crossNum
+    crossNum,
+    toChineseMoney
 };
