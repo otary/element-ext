e8c239b70118e025e3fa5b85478d7c521b9cd0c0
chenzw <656469722@qq.com>
2022-02-25 14:54:19 +0800

    update

--- src/util/url.js
+++ src/util/url.js
@@ -61,11 +61,49 @@ function createQueryString(oParam = {}) {
 }
 
 /**
+ * 解析url
+ * @param url
+ * @returns {{path: string, protocol: string, file, port: string, query: string, host: string, params: {}, url, hash: string, relative, segments: string[]}}
+ * @example parseUrl("https://www.baidu.com:80/s/index.html?ie=utf-8&f=8") => {"url":"https://www.baidu.com:80/s/index.html?ie=utf-8&f=8","protocol":"https","host":"www.baidu.com","port":"80","query":"?ie=utf-8&f=8","params":{"ie":"utf-8","f":"8"},"file":"index.html","hash":"","path":"/s/index.html","relative":"/s/index.html?ie=utf-8&f=8","segments":["s","index.html"]}
+ */
+function parseUrl(url) {
+    const ael = document.createElement("a");
+    ael.href = url;
+
+    return {
+        url,
+        protocol: ael.protocol.replace(':', ''),
+        host: ael.hostname,
+        port: ael.port,
+        query: ael.search,
+        params: (function () {
+            let ret = {},
+                seg = ael.search.replace(/^\?/, '').split('&'),
+                len = seg.length, i = 0, s;
+            for (; i < len; i++) {
+                if (!seg[i]) {
+                    continue;
+                }
+                s = seg[i].split('=');
+                ret[s[0]] = s[1];
+            }
+            return ret;
+        })(),
+        file: (ael.pathname.match(/\/([^\/?#]+)$/i) || [, ''])[1],
+        hash: ael.hash.replace('#', ''),
+        path: ael.pathname.replace(/^([^\/])/, '/$1'),
+        relative: (ael.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ''])[1],
+        segments: ael.pathname.replace(/^\//, '').split('/')
+    }
+}
+
+/**
  * @author chenzw
  */
 module.exports = {
     getUriParam,
     getUriParams,
     existsParam,
-    createQueryString
+    createQueryString,
+    parseUrl
 };
